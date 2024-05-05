141209ebf3bdfbcf977db72fae9c9fe44ae19394
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-05-05 16:53:16 +0300

    editing (wip)

--- frontend/src/items/api/items.js
+++ frontend/src/items/api/items.js
@@ -28,6 +28,30 @@ export const createItem = async ({ title, price, description, image, owner, toke
     return await res.json();
 };
 
+export const editItem = async ({ title, price, description, image, owner, token }) => {
+    const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/items`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                title,
+                price,
+                description,
+                image,
+                owner
+            })
+        }
+    );
+
+    return await res.json();
+};
+
+
 
 export const deleteItem = async ({ id, token }) => {
     const res = await fetch(
--- frontend/src/items/components/ItemsItem.jsx
+++ frontend/src/items/components/ItemsItem.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useRef, useState, useContext } from "react";
 import { useMutation } from 'react-query';
 
 import Card from '../../shared/components/Card';
 import Button from "../../shared/components/Button";
 import Modal from "../../shared/components/Modal";
+import Input from "../../shared/components/Input";
 
 import './ItemsItem.css';
 import { AuthContext } from "../../shared/context/auth-context";
@@ -15,6 +16,17 @@ const ItemsItem = props => {
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const showConfirmationHandler = () => setShowConfirmationModal(true);
   const cancelConfirmationHandler = () => setShowConfirmationModal(false);
+
+  const [showEditModal, setShowEditModal] = useState(false);
+  const showEditHandler = () => setShowEditModal(true);
+  const cancelEditHandler = () => setShowEditModal(false);
+
+
+  const titleRef = useRef(props.title);
+  const priceRef = useRef(props.price);
+  const descriptionRef = useRef(props.description);
+  const imageRef = useRef(props.image);
+
 
   const deleteItemMutation = useMutation({
     mutationFn: deleteItem,
@@ -34,9 +46,53 @@ const ItemsItem = props => {
     })
   }
 
+  const editItemMutation = useMutation({
+    mutationFn: deleteItem,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error)
+    }
+  })
+
+  const editConfirmedHandler = () => {
+    setShowConfirmationModal(false);
+    editItemMutation.mutate({
+      id: props.id,
+      title: titleRef.current.value,
+      price: priceRef.current.value,
+      description: descriptionRef.current.value,
+      image: imageRef.current.value,
+      owner: auth.userName,
+      token: auth.token
+    })
+  }
+
 
   return(
   <>
+      <Modal
+      show={showEditModal}
+      header="Editing?"
+      footerClass="place-item__modal-actions"
+      footer={
+        <React.Fragment>
+              <form className="item-form" onSubmit={editConfirmedHandler}>
+                <Input id="title" ref={titleRef} type="text" label="Title" defaultValue="test" key="test"/>
+                <Input id="price" ref={priceRef} type="text" label="Price" />
+                <Input id="description" ref={descriptionRef} type="text" label="Description" />
+                <Input id="image" ref={imageRef} type="text" label="Image Link" />
+                <Button type="submit">
+                  Edit Item
+                </Button>
+              </form>
+              <Button inverse onClick={cancelEditHandler}>Cancel</Button>
+        </React.Fragment>
+      }
+    >
+      <p>Do you want to edit?</p>
+    </Modal>
     <Modal
       show={showConfirmationModal}
       header="Are you sure?"
@@ -50,6 +106,7 @@ const ItemsItem = props => {
     >
       <p>Are you sure? Once it's gone, it's gone!</p>
     </Modal>
+
     <li className="item-item">
       <Card className="item-item__content">
         <div className="item-item__image">
@@ -60,8 +117,11 @@ const ItemsItem = props => {
         </div>
         <div className="item-item_actions">
 
-          <Button>Edit</Button>
-          {auth.isLoggedIn && (
+        
+        {auth.isLoggedIn && auth.userName == props.owner && (
+            <Button edit onClick={showEditHandler}>Edit</Button>
+          )}
+          {auth.isLoggedIn && auth.userName == props.owner && (
             <Button danger onClick={showConfirmationHandler}>Delete</Button>
           )}
         </div>
